89ed64391365c31e4a4a43fd38b7b3e98e558b1c
heyhassanbilal <hassanbilal1837@gmail.com>
2025-03-04 19:51:24 +0100

    Security Config test

--- src/main/java/com/example/fullstack/security/config/SecurityConfig.java
+++ src/main/java/com/example/fullstack/security/config/SecurityConfig.java
@@ -1,3 +1,87 @@
+//package com.example.fullstack.security.config;
+//import com.example.fullstack.security.filter.JwtValidationFilter;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.AuthenticationProvider;
+//import org.springframework.security.authentication.ProviderManager;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//
+//import java.util.List;
+//
+//@Configuration
+//@EnableMethodSecurity
+//public class SecurityConfig {
+//
+//
+//    private final UserDetailsService userDetailsService;
+//    private final JwtValidationFilter jwtValidationFilter;
+//
+//    public SecurityConfig(UserDetailsService userDetailsService, JwtValidationFilter jwtValidationFilter) {
+//        this.userDetailsService = userDetailsService;
+//        this.jwtValidationFilter = jwtValidationFilter;
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http
+//                //    .httpBasic(Customizer.withDefaults())
+//                .cors(cors->    cors.configurationSource(corsConfigurationSource()))
+//                .addFilterBefore(jwtValidationFilter, UsernamePasswordAuthenticationFilter.class)
+////                .authorizeHttpRequests(auth -> auth
+////                        .requestMatchers("/api/menuItem/").hasRole("USER")
+////                        .anyRequest().permitAll())
+//                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .build();
+//    }
+//
+//    @Bean
+//    public AuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//        authProvider.setPasswordEncoder(passwordEncoder());
+//        authProvider.setUserDetailsService(userDetailsService);
+//        return authProvider;
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+//        return authenticationConfiguration.getAuthenticationManager();
+//    }
+//
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // Allow frontend
+//        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+//        configuration.setAllowCredentials(true); // Allow cookies/authentication
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/", configuration);
+//        return source;
+//    }
+//}
+
+
 package com.example.fullstack.security.config;
 import com.example.fullstack.security.filter.JwtValidationFilter;
 import org.springframework.context.annotation.Bean;
@@ -42,9 +126,11 @@ public class SecurityConfig {
                 .cors(cors->    cors.configurationSource(corsConfigurationSource()))
                 .addFilterBefore(jwtValidationFilter, UsernamePasswordAuthenticationFilter.class)
 //                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/api/menuItem/").hasRole("USER")
+//                        .requestMatchers("/api/menuItem/**").hasRole("USER")
 //                        .anyRequest().permitAll())
+
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
@@ -76,7 +162,7 @@ public class SecurityConfig {
         configuration.setAllowCredentials(true); // Allow cookies/authentication
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/", configuration);
+        source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 }
